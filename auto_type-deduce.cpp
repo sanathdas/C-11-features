aed20fbebd333486ad9b1129350bceba4a64a17b
sanathdas <sanath.srs@gmail.com>
2022-02-04 18:44:40 +0530

    C++11 auto keyword usage

---
+++ auto_type-deduce.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //cout<<"Hello World";
+    
+    auto Integer_ = 10;
+    
+    auto Float_ = 2.5;
+    
+    cout << "int_val =  " <<Integer_ << "\t float_value = " <<Float_ << endl;
+
+    return 0;
+}
