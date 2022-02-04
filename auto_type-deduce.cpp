7022584d609596604cb20774504b27307a1bcbeb
sanathdas <sanath.srs@gmail.com>
2022-02-04 18:45:54 +0530

    Update auto_type-deduce.cpp

--- auto_type-deduce.cpp
+++ auto_type-deduce.cpp
@@ -6,9 +6,9 @@ int main()
 {
     //cout<<"Hello World";
     
-    auto Integer_ = 10;
+    auto Integer_ = 10;//type deduced as int
     
-    auto Float_ = 2.5;
+    auto Float_ = 2.5;////type deduced as float
     
     cout << "int_val =  " <<Integer_ << "\t float_value = " <<Float_ << endl;
 
